183b44bc945e94723964dcaa08c96604db8b9238
nopravlem <pravtadikonda410@gmail.com>
2020-05-16 06:03:13 -0400

    create db structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'ingredients/index'
-  get 'ingredients/show'
-  get 'products/index'
-  get 'products/show'
-  get 'companies/index'
   resources :companies, only: [:index, :show] do
     resources :products, only: [:index, :show]
   end
